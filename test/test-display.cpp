f436bec332db12fa1bd23f240c7559368b690a3a
cfreeman01 <cfreeman10538@gmail.com>
2024-03-13 20:04:13 -0400

    add more display tests

--- test/test-display.cpp
+++ test/test-display.cpp
@@ -21,6 +21,10 @@ TEST_GROUP(DisplayTests){
 
 TEST(DisplayTests, WriteAll1Middle)
 {
+    const uint32_t spriteHeight = 8;
+    const uint32_t spriteX      = 24;
+    const uint32_t spriteY      = 8;
+
     uint64_t expected[Display::NUM_ROWS] = 
     {
         0x00'00'00'00'00'00'00'00,
@@ -42,9 +46,6 @@ TEST(DisplayTests, WriteAll1Middle)
         //rest zero
     };
 
-    const uint32_t spriteHeight = 8;
-    const uint32_t spriteX      = 24;
-    const uint32_t spriteY      = 8;
     uint8_t sprite[spriteHeight] = 
     {
         0xFF,
@@ -58,9 +59,171 @@ TEST(DisplayTests, WriteAll1Middle)
     };
 
     bool ret = display.drawSprite(spriteX, spriteY, sprite, spriteHeight);
-    CHECK_EQUAL(0, ret);
+    CHECK_EQUAL(false, ret);
     MEMCMP_EQUAL(expected, display.getDisplayData(), displaySizeBytes);
 
     display.clear();
     MEMCMP_EQUAL(empty, display.getDisplayData(), displaySizeBytes);
+}
+
+TEST(DisplayTests, WritePatternMiddle)
+{
+    const uint32_t spriteHeight = 8;
+    const uint32_t spriteX      = 24;
+    const uint32_t spriteY      = 8;
+
+    uint64_t expected[Display::NUM_ROWS] = 
+    {
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'88'00'00'00'00,
+        0x00'00'00'88'00'00'00'00,
+        0x00'00'00'88'00'00'00'00,
+        0x00'00'00'88'00'00'00'00,
+        0x00'00'00'88'00'00'00'00,
+        0x00'00'00'88'00'00'00'00,
+        0x00'00'00'88'00'00'00'00,
+        0x00'00'00'88'00'00'00'00,
+        //rest zero
+    };
+
+    uint8_t sprite1[spriteHeight] = 
+    {
+        0xAA,
+        0xAA,
+        0xAA,
+        0xAA,
+        0xAA,
+        0xAA,
+        0xAA,
+        0xAA
+    };
+
+    uint8_t sprite2[spriteHeight] = 
+    {
+        0x22,
+        0x22,
+        0x22,
+        0x22,
+        0x22,
+        0x22,
+        0x22,
+        0x22
+    };
+
+    bool ret = display.drawSprite(spriteX, spriteY, sprite1, spriteHeight);
+    CHECK_EQUAL(false, ret);
+
+    ret = display.drawSprite(spriteX, spriteY, sprite2, spriteHeight);
+    CHECK_EQUAL(true, ret);
+    MEMCMP_EQUAL(expected, display.getDisplayData(), displaySizeBytes);
+}
+
+TEST(DisplayTests, WriteAll1RightEdge)
+{
+    const uint32_t spriteHeight = 8;
+    const uint32_t spriteX      = 60;
+    const uint32_t spriteY      = 8;
+
+    uint64_t expected[Display::NUM_ROWS] = 
+    {
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'0F,
+        0x00'00'00'00'00'00'00'0F,
+        0x00'00'00'00'00'00'00'0F,
+        0x00'00'00'00'00'00'00'0F,
+        0x00'00'00'00'00'00'00'0F,
+        0x00'00'00'00'00'00'00'0F,
+        0x00'00'00'00'00'00'00'0F,
+        0x00'00'00'00'00'00'00'0F,
+        //rest zero
+    };
+
+    uint8_t sprite[spriteHeight] = 
+    {
+        0xFF,
+        0xFF,
+        0xFF,
+        0xFF,
+        0xFF,
+        0xFF,
+        0xFF,
+        0xFF
+    };
+
+    bool ret = display.drawSprite(spriteX, spriteY, sprite, spriteHeight);
+    CHECK_EQUAL(false, ret);
+    MEMCMP_EQUAL(expected, display.getDisplayData(), displaySizeBytes);
+}
+
+TEST(DisplayTests, WriteAll1BottomEdge)
+{
+    const uint32_t spriteHeight = 8;
+    const uint32_t spriteX      = 24;
+    const uint32_t spriteY      = 28;
+
+    uint64_t expected[Display::NUM_ROWS] = 
+    {
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'00'00'00'00'00,
+        0x00'00'00'FF'00'00'00'00,
+        0x00'00'00'FF'00'00'00'00,
+        0x00'00'00'FF'00'00'00'00,
+        0x00'00'00'FF'00'00'00'00,
+    };
+
+    uint8_t sprite[spriteHeight] = 
+    {
+        0xFF,
+        0xFF,
+        0xFF,
+        0xFF,
+        0xFF,
+        0xFF,
+        0xFF,
+        0xFF
+    };
+
+    bool ret = display.drawSprite(spriteX, spriteY, sprite, spriteHeight);
+    CHECK_EQUAL(false, ret);
+    MEMCMP_EQUAL(expected, display.getDisplayData(), displaySizeBytes);
 }
